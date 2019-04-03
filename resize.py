f087c23b63f8357f84a1914aa0ca300de4212ca3
Jeffrey <jeffrey950310@gmail.com>
2019-04-03 16:10:25 +0800

    Add folder traversal to convert all dcm to png

--- resize.py
+++ resize.py
@@ -2,15 +2,16 @@ import cv2
 import numpy as np
 import os
 import pydicom
+import glob
 
 W = 400.
 
-filename = '000000.dcm'
-ds = pydicom.read_file(filename)
-ori_img = ds.pixel_array
-height, width = ori_img.shape
-imgScale = W/width
-newX, newY = ori_img.shape[1] * imgScale, ori_img.shape[0] * imgScale
-newImg = cv2.resize(ori_img, (int(newX), int(newY)))
-newImg = newImg[200:600, 0:400]
-cv2.imwrite("000000.png", newImg)
+for filename in glob.iglob('**/*.dcm', recursive=True):
+	ds = pydicom.read_file(filename)
+	ori_img = ds.pixel_array
+	height, width = ori_img.shape
+	imgScale = W/width
+	newX, newY = ori_img.shape[1] * imgScale, ori_img.shape[0] * imgScale
+	newImg = cv2.resize(ori_img, (int(newX), int(newY)))
+	newImg = newImg[200:600, 0:400]
+	cv2.imwrite(filename.replace(".dcm", ".png"), newImg)
